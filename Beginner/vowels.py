dd7000beffbf9d4f07e27ccd610ce8f5978d49de
ReenaSubanandharaj <41002420+ReenaSubanandharaj@users.noreply.github.com>
2018-09-03 12:02:25 +0530

    Update vowels.py

--- Beginner/vowels.py
+++ Beginner/vowels.py
@@ -2,9 +2,9 @@ z=['a','e','i','o','u']
 print("enter the value")
 x=input()
 if(x.isalpha()):
-  if(x in z):
-    print("vowel")
-	else:
-		print("consonant")
+    if(x in z):
+        print("vowel")
+    else:
+	print("consonant")
 else:
 	print("invalid")
